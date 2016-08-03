fd537d8b1de70114aa97372e38c5ca00b0b32420
vitaliykobrin <vitaliy.kobrin@gmail.com>
2016-08-03 15:57:38 +0300

    add colors

--- src/main/java/CalendarPrinter.java
+++ src/main/java/CalendarPrinter.java
@@ -7,7 +7,14 @@ import java.util.stream.IntStream;
 public class CalendarPrinter {
 
     public static final Locale LOCALE = Locale.ENGLISH;
-    public static final String HORIZONTAL_LINE_SPLITTER = "\n---------------------------------------";
+    public static final ZoneId ZONE_ID = ZoneId.of(ZoneId.SHORT_IDS.get("ART"));
+    public static final ZonedDateTime currentDate = ZonedDateTime.now(ZONE_ID);
+
+    public static final String HORIZONTAL_LINE_DELIMITER = "\n----------------------------------------";
+    public static final String WEEKEND_COLOR = "\u001B[31m";
+    public static final String CURRENT_DATE_COLOR = "\u001B[36m";
+    public static final String DEFAULT_COLOR = "\u001B[0m";
+    public static final String WEEK_NUMBER_COLOR = "\u001B[32m";
 
     public static void printMonth(int year, int month) {
         ZonedDateTime zonedDateTime = createZonedDateTime(year, month);
@@ -17,41 +24,45 @@ public class CalendarPrinter {
         final int[] weekOfYear = {zonedDateTime.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)};
         String startMonthLine = createStartMonthLine(startDayOfWeek);
 
-        printDaysOfWeek();
+        printFirstLine();
         final int[] dayOfWeek = {startDayOfWeek};
-        IntStream.rangeClosed(1, monthLength).forEach(i -> {
+        for (int i = 1; i <= monthLength; i++) {
             if (i == 1) {
                 printWeekNumber(weekOfYear);
                 System.out.print(startMonthLine);
             }
-            printDay(i);
+            printDay(i, dayOfWeek[0]);
             if (dayOfWeek[0] % 7 == 0) {
                 createNewLine(weekOfYear, dayOfWeek);
             }
             dayOfWeek[0]++;
-        });
+        }
     }
 
     private static void createNewLine(int[] weekOfYear, int[] dayOfWeek) {
         dayOfWeek[0] = 0;
-        System.out.println(HORIZONTAL_LINE_SPLITTER);
+        System.out.println(DEFAULT_COLOR + HORIZONTAL_LINE_DELIMITER);
         printWeekNumber(weekOfYear);
     }
 
     private static void printWeekNumber(int[] weekOfYear) {
-        System.out.print(weekOfYear[0] + "  | ");
+        System.out.print(WEEK_NUMBER_COLOR + weekOfYear[0] + "  | " + DEFAULT_COLOR);
         weekOfYear[0]++;
     }
 
-    private static void printDay(int i) {
+    private static void printDay(int i, int dayOfWeek) {
         String dayOfMonth = String.valueOf(i);
         dayOfMonth = dayOfMonth.length() == 1 ? " " + dayOfMonth : dayOfMonth;
+        dayOfMonth = dayOfWeek > 5 ? WEEKEND_COLOR + dayOfMonth : DEFAULT_COLOR + dayOfMonth;
         System.out.print(dayOfMonth + " | ");
     }
 
     private static ZonedDateTime createZonedDateTime(int year, int month) {
-        ZoneId zone = ZoneId.of(ZoneId.SHORT_IDS.get("ART"));
-        return ZonedDateTime.of(year, month, 1, 0, 0, 0, 0, zone);
+        return ZonedDateTime.of(year, month, 1, 0, 0, 0, 0, ZONE_ID);
+    }
+
+    private static ZonedDateTime createZonedDateTime(int year, int month, int day) {
+        return ZonedDateTime.of(year, month, day, 0, 0, 0, 0, ZONE_ID);
     }
 
     private static String createStartMonthLine(int startDayOfWeek) {
@@ -60,13 +71,13 @@ public class CalendarPrinter {
         return startLine[0];
     }
 
-    private static void printDaysOfWeek() {
-        System.out.print("Week| ");
+    private static void printFirstLine() {
+        System.out.print(WEEK_NUMBER_COLOR + "Week| ");
         IntStream.rangeClosed(1, 7).forEach(i -> {
                 DayOfWeek dayOfWeek = DayOfWeek.of(i);
                 String day = dayOfWeek.getDisplayName(TextStyle.SHORT, LOCALE) + "| ";
-                System.out.print(day);
+                System.out.print(i > 5 ? WEEKEND_COLOR + day : DEFAULT_COLOR + day);
         });
-        System.out.println(HORIZONTAL_LINE_SPLITTER);
+        System.out.println(DEFAULT_COLOR + HORIZONTAL_LINE_DELIMITER);
     }
 }
