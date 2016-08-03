9ea4a15b2250250fa5ac83c21c3ed8a75c2a6373
vitaliykobrin <vitaliy.kobrin@gmail.com>
2016-08-03 15:38:54 +0300

    add week numbers

--- src/main/java/CalendarPrinter.java
+++ src/main/java/CalendarPrinter.java
@@ -1,34 +1,46 @@
-import java.time.DayOfWeek;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.TextStyle;
+import java.time.temporal.IsoFields;
 import java.util.*;
 import java.util.stream.IntStream;
 
 public class CalendarPrinter {
 
     public static final Locale LOCALE = Locale.ENGLISH;
+    public static final String HORIZONTAL_LINE_SPLITTER = "\n---------------------------------------";
 
     public static void printMonth(int year, int month) {
         ZonedDateTime zonedDateTime = createZonedDateTime(year, month);
         int startDayOfWeek = zonedDateTime.getDayOfWeek().getValue();
-        int monthLength = zonedDateTime.getMonth().length(true);
+        boolean isLeap = Year.isLeap(zonedDateTime.getYear());
+        int monthLength = zonedDateTime.getMonth().length(isLeap);
+        final int[] weekOfYear = {zonedDateTime.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)};
         String startMonthLine = createStartMonthLine(startDayOfWeek);
 
         printDaysOfWeek();
         final int[] dayOfWeek = {startDayOfWeek};
         IntStream.rangeClosed(1, monthLength).forEach(i -> {
             if (i == 1) {
+                printWeekNumber(weekOfYear);
                 System.out.print(startMonthLine);
             }
             printDay(i);
             if (dayOfWeek[0] % 7 == 0) {
-                System.out.println();
-                dayOfWeek[0] = 0;
-                System.out.println("----------------------------------");
+                createNewLine(weekOfYear, dayOfWeek);
             }
             dayOfWeek[0]++;
         });
+    }
+
+    private static void createNewLine(int[] weekOfYear, int[] dayOfWeek) {
+        dayOfWeek[0] = 0;
+        System.out.println(HORIZONTAL_LINE_SPLITTER);
+        printWeekNumber(weekOfYear);
+    }
+
+    private static void printWeekNumber(int[] weekOfYear) {
+        System.out.print(weekOfYear[0] + "  | ");
+        weekOfYear[0]++;
     }
 
     private static void printDay(int i) {
@@ -49,11 +61,12 @@ public class CalendarPrinter {
     }
 
     private static void printDaysOfWeek() {
+        System.out.print("Week| ");
         IntStream.rangeClosed(1, 7).forEach(i -> {
                 DayOfWeek dayOfWeek = DayOfWeek.of(i);
                 String day = dayOfWeek.getDisplayName(TextStyle.SHORT, LOCALE) + "| ";
                 System.out.print(day);
         });
-        System.out.println();
+        System.out.println(HORIZONTAL_LINE_SPLITTER);
     }
 }
